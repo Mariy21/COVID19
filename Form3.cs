351638b3b8a3996b678efbf992f74ac58a5e5253
Mariy21 <60806554+Mariy21@users.noreply.github.com>
2020-06-05 11:53:11 +0300

    Add files via upload

---
+++ Form3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2.ActiveForm.Hide();
+
+            Form1 frm1 = new Form1();
+
+            frm1.Show();
+        
+    }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+                textBox1.Show();
+            else textBox1.Hide();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+                textBox3.Show();
+            else textBox3.Hide();
+        }
+    }
+}
